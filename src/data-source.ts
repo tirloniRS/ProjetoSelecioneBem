59708cd6f64d736d4f621c2ad72c788789813ed7
Alexsandro Tirloni <tirloni.rs@gmail.com>
2023-05-13 21:36:16 -0300

    DB_URI

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,18 @@ const port = process.env.DB_PORT as number | undefined
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    url: process.env.DB_URI,    
+
+    // conexão nuvem
+    url: process.env.DB_URI,
+
+
+    // conexão local    
+    // host: process.env.DB_HOST,
+    // port: port,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+
     entities: [`${__dirname}/**/entities/*.{ts,js}`],
     migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 })
