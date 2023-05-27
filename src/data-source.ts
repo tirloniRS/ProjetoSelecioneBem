a906b9cbc7f6856860472a232b8966854120c6d6
Alexsandro Tirloni <tirloni.rs@gmail.com>
2023-05-27 01:49:22 -0300

    Testando alteração no deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,23 @@
-import 'dotenv/config'
-import 'reflect-metadata'
-import { DataSource } from "typeorm"
+import "dotenv/config";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
 
-const port = process.env.DB_PORT as number | undefined
+const port = process.env.DB_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
+  //bd
+  type: "postgres",
 
-    type: 'postgres',
+  // CÓDIGO PARA CONEXÃO NA NUVEM - COMENTAR CONEXÃO LOCAL
+  url: process.env.DB_URI,
 
-    // CÓDIGO PARA CONEXÃO NA NUVEM - COMENTAR CONEXÃO LOCAL
-    url: process.env.DB_URI,
+  // CÓDIGO PARA CONEXÃO LOCAL, COMENTAR CONEXÃO NUVEM
+  // host: process.env.DB_HOST,
+  // port: port,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
 
-    // CÓDIGO PARA CONEXÃO LOCAL, COMENTAR CONEXÃO NUVEM
-    // host: process.env.DB_HOST,
-    // port: port,
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME,
-
-    entities: [`${__dirname}/**/entities/*.{ts,js}`],
-    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-})
+  entities: [`${__dirname}/**/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+});
